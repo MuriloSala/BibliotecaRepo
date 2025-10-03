8da4296ff5d483a90e034f2acc97b4621ed10401
Murilo Sala <msala.etec@gmail.com>
2025-10-03 10:09:40 -0300

    Adicionado classe principal

---
+++ src/br/com/etec/biblioteca/cadastro/classeInformacoes.java
@@ -0,0 +1,22 @@
+package br.com.etec.biblioteca.cadastro;
+
+public class classeInformacoes {
+	
+	String tituloAt;
+	String subtituloAt;
+	int numeroPaginasAt;
+	String autoresAt;
+	String generosAt;
+	
+	
+	public classeInformacoes(){
+		
+	}
+	
+	public classeInformacoes(String autor, String titulo){
+		this.autoresAt = autor;
+		this.
+		
+	}
+
+}
